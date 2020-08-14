84de203ae7fb58a758e3e6f7bce51071d4a1eb77
EmotionalLove <please@someonelove.me>
2020-08-14 13:09:10 -0600

    kamiui.java & checklist

---
+++ src/main/java/me/zeroeightsix/kami/ui/KamiUi.java
@@ -0,0 +1,18 @@
+package me.zeroeightsix.kami.ui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+/**
+ * @author Sasha
+ * <p>
+ * This is where the code for the new KAMI ClickUI will live.
+ * <p>
+ * Key points:
+ * - New user interface. Ordered my most often toggled modules by default.
+ * - Searchbar, sort by relevancy
+ * - Icons for modules(?)
+ * - Simplisitic and lightweight, don't overdo it.
+ * - Use Fiber for storing settings (depending on what settings there are).
+ */
+public class KamiUi extends GuiScreen {
+}
